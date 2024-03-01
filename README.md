3b37c909d08922d5b66ed8d794bf64b8cd41ca0a
NicN <nicn.nlab@gmail.com>
2024-03-01 13:07:11 +0100

    Define, initialise and print a classic C struct

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A collection of files containing some code made just to try Rust features in the goal to better understand them
 
 ## Current Task 
-**Task 0** : Print a simple helloWorld message
+**Task 1** : Define, initialise and print a classic C struct
 
 ## Compile & Run
 With Rust 1.76 On Ubuntu
@@ -11,3 +11,5 @@ With Rust 1.76 On Ubuntu
     $ rustc main.rs -o  ./target/main    #compile
     $ ./target/main                      #run
 
+## Previous Task 
+**Task 0** : Print a simple helloWorld message
--- main.rs
+++ main.rs
@@ -1,3 +1,25 @@
+struct Person {
+    id:u32,
+    name:String,
+    gender:String
+} 
+
 fn main() {
-    println!("Hello, world!");
+
+    let person_1 = Person {
+        id:1,
+        name:"Paul Abbas".to_string(),
+        gender:"xmale".to_string()
+    };
+
+    let gender_message = if person_1.gender.eq("male") {
+        "I am a boy"
+    } else if person_1.gender.eq("female") {
+        "I am a girl"
+    } else {
+        "gender:unknown"
+    };
+
+
+    println!("Hello world I am {}, {}!", person_1.name, gender_message);
 }
